aabb487fd09889f98d35b8beb6889344719d043d
Kuba Mazurkiewicz <kmazurki@cisco.com>
2024-01-27 23:45:51 +0100

    added active_directory resources

--- ise_identity_management.tf
+++ ise_identity_management.tf
@@ -53,3 +53,108 @@ resource "ise_certificate_authentication_profile" "certificate_authentication_pr
   match_mode                   = try(each.value.match_mode, local.defaults.ise.identity_management.certificate_authentication_profiles.description, null)
   username_from                = try(each.value.username_from, local.defaults.ise.identity_management.certificate_authentication_profiles.description, null)
 }
+
+resource "ise_active_directory_join_point" "active_directory_join_point" {
+  for_each = { for ad in try(local.ise.identity_management.active_directory, []) : ad.name => ad if var.manage_identity_management }
+
+  name                       = each.key
+  description                = try(each.value.description, local.defaults.ise.identity_management.active_directory.description, null)
+  domain                     = try(each.value.domain, local.defaults.ise.identity_management.active_directory.domain, null)
+  ad_scopes_names            = try(each.value.ad_scopes_names, local.defaults.ise.identity_management.active_directory.ad_scopes_names, null)
+  enable_domain_allowed_list = try(each.value.enable_domain_allowed_list, local.defaults.ise.identity_management.active_directory.enable_domain_allowed_list, null)
+  groups                     = []
+  attributes = [for attr in try(each.value.attributes, []) : {
+    name          = try(attr.name, null)
+    type          = try(attr.type, local.defaults.ise.identity_management.active_directory.attributes.type, null)
+    internal_name = try(attr.internal_name, local.defaults.ise.identity_management.active_directory.attributes.internal_name, null)
+    default_value = try(attr.default_value, local.defaults.ise.identity_management.active_directory.attributes.default_value, null)
+  }]
+  rewrite_rules = [for rule in try(each.value.rewrite_rules, []) : {
+    row_id         = try(rule.row_id, local.defaults.ise.identity_management.active_directory.rewrite_rules.row_id, null)
+    rewrite_match  = try(rule.rewrite_match, local.defaults.ise.identity_management.active_directory.rewrite_rules.rewrite_match, null)
+    rewrite_result = try(rule.rewrite_result, local.defaults.ise.identity_management.active_directory.rewrite_rules.rewrite_result, null)
+  }]
+  enable_rewrites                   = try(each.value.enable_rewrites, local.defaults.ise.identity_management.active_directory.enable_rewrites, null)
+  enable_pass_change                = try(each.value.enable_pass_change, local.defaults.ise.identity_management.active_directory.enable_pass_change, null)
+  enable_machine_auth               = try(each.value.enable_machine_auth, local.defaults.ise.identity_management.active_directory.enable_machine_auth, null)
+  enable_machine_access             = try(each.value.enable_machine_access, local.defaults.ise.identity_management.active_directory.enable_machine_access, null)
+  enable_dialin_permission_check    = try(each.value.enable_dialin_permission_check, local.defaults.ise.identity_management.active_directory.enable_dialin_permission_check, null)
+  plaintext_auth                    = try(each.value.plaintext_auth, local.defaults.ise.identity_management.active_directory.plaintext_auth, null)
+  aging_time                        = try(each.value.aging_time, local.defaults.ise.identity_management.active_directory.aging_time, null)
+  enable_callback_for_dialin_client = try(each.value.enable_callback_for_dialin_client, local.defaults.ise.identity_management.active_directory.enable_callback_for_dialin_client, null)
+  identity_not_in_ad_behaviour      = try(each.value.identity_not_in_ad_behaviour, local.defaults.ise.identity_management.active_directory.identity_not_in_ad_behaviour, null)
+  unreachable_domains_behaviour     = try(each.value.unreachable_domains_behaviour, local.defaults.ise.identity_management.active_directory.unreachable_domains_behaviour, null)
+  schema                            = try(each.value.schema, local.defaults.ise.identity_management.active_directory.schema, null)
+  first_name                        = try(each.value.first_name, local.defaults.ise.identity_management.active_directory.first_name, null)
+  department                        = try(each.value.department, local.defaults.ise.identity_management.active_directory.department, null)
+  last_name                         = try(each.value.last_name, local.defaults.ise.identity_management.active_directory.last_name, null)
+  organizational_unit               = try(each.value.organizational_unit, local.defaults.ise.identity_management.active_directory.organizational_unit, null)
+  job_title                         = try(each.value.job_title, local.defaults.ise.identity_management.active_directory.job_title, null)
+  locality                          = try(each.value.locality, local.defaults.ise.identity_management.active_directory.locality, null)
+  email                             = try(each.value.email, local.defaults.ise.identity_management.active_directory.email, null)
+  state_or_province                 = try(each.value.state_or_province, local.defaults.ise.identity_management.active_directory.state_or_province, null)
+  telephone                         = try(each.value.telephone, local.defaults.ise.identity_management.active_directory.telephone, null)
+  country                           = try(each.value.country, local.defaults.ise.identity_management.active_directory.country, null)
+  street_address                    = try(each.value.street_address, local.defaults.ise.identity_management.active_directory.street_address, null)
+  enable_failed_auth_protection     = try(each.value.enable_failed_auth_protection, local.defaults.ise.identity_management.active_directory.enable_failed_auth_protection, null)
+  failed_auth_threshold             = try(each.value.failed_auth_threshold, local.defaults.ise.identity_management.active_directory.failed_auth_threshold, null)
+  auth_protection_type              = try(each.value.auth_protection_type, local.defaults.ise.identity_management.active_directory.auth_protection_type, null)
+}
+
+resource "ise_active_directory_join_domain_with_all_nodes" "active_directory_join_domain_with_all_nodes" {
+  for_each = { for ad in try(local.ise.identity_management.active_directory, []) : ad.name => ad if var.manage_identity_management }
+
+  join_point_id = ise_active_directory_join_point.active_directory_join_point[each.key].id
+  additional_data = [
+    {
+      name  = "username"
+      value = try(each.value.ad_username, local.defaults.ise.identity_management.active_directory.ad_username, null)
+    },
+    {
+      name  = "password"
+      value = try(each.value.ad_password, local.defaults.ise.identity_management.active_directory.ad_password, null)
+    }
+  ]
+
+  depends_on = [ise_active_directory_join_point.active_directory_join_point]
+}
+
+data "ise_active_directory_groups_by_domain" "all_groups" {
+  for_each = { for ad in try(local.ise.identity_management.active_directory, []) : ad.name => ad if var.manage_identity_management }
+
+  join_point_id = ise_active_directory_join_point.active_directory_join_point[each.key].id
+  domain        = try(each.value.domain, local.defaults.ise.identity_management.active_directory.domain, null)
+
+  depends_on = [ise_active_directory_join_point.active_directory_join_point, ise_active_directory_join_domain_with_all_nodes.active_directory_join_domain_with_all_nodes]
+}
+
+locals {
+  active_directory_groups_all = {
+    for k, v in ise_active_directory_groups_by_domain.all_groups :
+    k => { for group in v.groups : group.name => group }
+  }
+
+  active_directory_groups = {
+    for ad in try(local.ise.identity_management.active_directory, []) : ad.name => [
+      for group in ad.groups : {
+        name = group
+        type = try(local.active_directory_groups_all[ad.name][group].type, null)
+        sid  = try(local.active_directory_groups_all[ad.name][group].sid, null)
+      }
+    ]
+  }
+}
+
+resource "ise_active_directory_add_groups" "active_directory_groups" {
+  for_each = { for ad in try(local.ise.identity_management.active_directory, []) : ad.name => ad if var.manage_identity_management }
+
+  join_point_id              = ise_active_directory_join_point.active_directory_join_point[each.key].id
+  name                       = ise_active_directory_join_point.active_directory_join_point[each.key].name
+  description                = ise_active_directory_join_point.active_directory_join_point[each.key].description
+  domain                     = ise_active_directory_join_point.active_directory_join_point[each.key].domain
+  ad_scopes_names            = ise_active_directory_join_point.active_directory_join_point[each.key].ad_scopes_names
+  enable_domain_allowed_list = ise_active_directory_join_point.active_directory_join_point[each.key].enable_domain_allowed_list
+  groups                     = try(local.active_directory_groups, local.defaults.ise.identity_management.active_directory.groups, null)
+
+  depends_on = [ise_active_directory_join_point.active_directory_join_point, ise_active_directory_join_domain_with_all_nodes.active_directory_join_domain_with_all_nodes]
+}

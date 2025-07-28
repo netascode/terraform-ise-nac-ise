56c5fa104dcca436decd93d47132aa0944f97c58
shumphr2 <shumphr2@cisco.com>
2025-07-28 21:55:02 +0900

    #28 add support for shell profile av-pairs

--- ise_network_access.tf
+++ ise_network_access.tf
@@ -78,7 +78,7 @@ resource "ise_allowed_protocols" "allowed_protocols" {
 }
 
 locals {
-  # Define patterns that should be treated as AttributeValue despite containing colons
+  # Define strings that should be treated as AttributeValue despite containing colons
   attribute_value_patterns = [
     "shell:priv-lvl=",     # Matches shell:priv-lvl=15 and similar
   ]
@@ -119,14 +119,14 @@ resource "ise_authorization_profile" "authorization_profile" {
   reauthentication_connectivity                         = try(each.value.reauthentication_connectivity, local.defaults.ise.network_access.policy_elements.authorization_profiles.reauthentication_connectivity, null)
   reauthentication_timer                                = try(each.value.reauthentication_timer, local.defaults.ise.network_access.policy_elements.authorization_profiles.reauthentication_timer, null)
   airespace_ipv6_acl                                    = try(each.value.airespace_ipv6_acl, local.defaults.ise.network_access.policy_elements.authorization_profiles.airespace_ipv6_acl, null)
-  advanced_attributes = try([for i in each.value.advanced_attributes : {
+  advanced_attributes = length(try(each.value.advanced_attributes, [])) > 0 ? [for i in each.value.advanced_attributes : {
     attribute_left_dictionary_name  = try(split(":", i.name)[0], null)
     attribute_left_name             = try(split(":", i.name)[1], null)
-    attribute_right_value_type      = anytrue([for pattern in local.attribute_value_patterns : can(regex(pattern, i.value))]) ? "AttributeValue" : (try(split(":", i.value)[1], null) != null ? "AdvancedDictionaryAttribute" : "AttributeValue")
-    attribute_right_dictionary_name = anytrue([for pattern in local.attribute_value_patterns : can(regex(pattern, i.value))]) ? null : (try(split(":", i.value)[1], null) != null ? split(":", i.value)[0] : null)
-    attribute_right_name            = anytrue([for pattern in local.attribute_value_patterns : can(regex(pattern, i.value))]) ? null : (try(split(":", i.value)[1], null) != null ? split(":", i.value)[1] : null)
-    attribute_right_value           = anytrue([for pattern in local.attribute_value_patterns : can(regex(pattern, i.value))]) ? i.value : (try(split(":", i.value)[1], null) != null ? null : i.value)
-  }], null)
+    attribute_right_value_type      = anytrue([for pattern in local.attribute_value_patterns : strcontains(i.value, pattern)]) ? "AttributeValue" : (try(split(":", i.value)[1], null) != null ? "AdvancedDictionaryAttribute" : "AttributeValue")
+    attribute_right_dictionary_name = anytrue([for pattern in local.attribute_value_patterns : strcontains(i.value, pattern)]) ? null : (try(split(":", i.value)[1], null) != null ? split(":", i.value)[0] : null)
+    attribute_right_name            = anytrue([for pattern in local.attribute_value_patterns : strcontains(i.value, pattern)]) ? null : (try(split(":", i.value)[1], null) != null ? split(":", i.value)[1] : null)
+    attribute_right_value           = anytrue([for pattern in local.attribute_value_patterns : strcontains(i.value, pattern)]) ? i.value : (try(split(":", i.value)[1], null) != null ? null : i.value)
+  }] : null
 
   lifecycle {
     create_before_destroy = true
